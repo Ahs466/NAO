6fd075d41b068af2a07c62bf6050072d1ded6e69
NevaDobric <neva.dobric@gmail.com>
2024-10-25 14:39:47 +0100

    CREATE: Main + Fizzbuzz

--- Main.java
+++ Main.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;  // Import the Scanner class
 public class Main {
-
+    public static void main(String[] args) {
+        Scanner requestNum = new Scanner(System.in);
+        System.out.printf("Insert number!");  // Import the Scanner class
+        int upperLimit = requestNum.nextInt();
+        for(int i = 0; i < upperLimit;i++){
+            System.out.printf("%s: ", i);
+            if(i % 3 == 0){
+                System.out.printf("Fizz");
+            }
+            if(i % 5 == 0){
+                System.out.printf("Buzz");
+            }
+            System.out.printf("\n");
+        }
+    }
 }
