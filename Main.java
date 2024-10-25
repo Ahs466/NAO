1f52d0484447bf6349f14c7ba60bd04359a36aea
Nathan <Nathan.Hayes@lloydsbanking.com>
2024-10-25 15:05:19 +0100

    CREATE: Did blackjack task

--- Main.java
+++ Main.java
@@ -1,18 +1,43 @@
 import java.util.Scanner;  // Import the Scanner class
 public class Main {
     public static void main(String[] args) {
-        Scanner requestNum = new Scanner(System.in);
-        System.out.printf("Insert number!");  // Import the Scanner class
-        int upperLimit = requestNum.nextInt();
-        for(int i = 0; i < upperLimit;i++){
-            System.out.printf("%s: ", i);
-            if(i % 3 == 0){
-                System.out.printf("Fizz");
+//        Scanner requestNum = new Scanner(System.in);
+//        System.out.printf("Insert number!");  // Import the Scanner class
+//        int upperLimit = requestNum.nextInt();
+//        for(int i = 0; i < upperLimit;i++){
+//            System.out.printf("%s: ", i);
+//            if(i % 3 == 0){
+//                System.out.printf("Fizz");
+//            }
+//            if(i % 5 == 0){
+//                System.out.printf("Buzz");
+//            }
+//            System.out.printf("\n");
+
+        Scanner scannerObject = new Scanner(System.in);
+        System.out.print("Please input number 1: ");
+        int num1 = scannerObject.nextInt();
+        System.out.print("Please input number 2: ");
+        int num2 = scannerObject.nextInt();
+        if (num1 > 21 && num2 > 21) {
+            System.out.println("Both are greater than 21!");
+        }
+        else {
+            if (num1 < num2) {
+                if (num2 <= 21)
+                    System.out.printf("%s wins!", num2);
+                else
+                    System.out.printf("%s wins!", num1);
             }
-            if(i % 5 == 0){
-                System.out.printf("Buzz");
+            else if (num2 < num1) {
+                if (num1 <= 21)
+                    System.out.printf("%s wins!", num1);
+                else
+                    System.out.printf("%s wins!", num2);
             }
-            System.out.printf("\n");
+            else {
+                System.out.println("Both are equal");
+            }
         }
     }
 }
